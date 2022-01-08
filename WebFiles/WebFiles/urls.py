840f119a0d1fd9bf8ad62a74eb53921df9e785fd
VergilX <abhinand.dmanoj@gmail.com>
2022-01-08 13:28:14 +0530

    Modified python files and added users app

--- WebFiles/WebFiles/urls.py
+++ WebFiles/WebFiles/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('website.urls')),
+    path('users/', include('users.urls'))
 ]
